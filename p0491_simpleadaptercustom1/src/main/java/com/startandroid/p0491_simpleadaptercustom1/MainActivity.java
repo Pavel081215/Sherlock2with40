1cccf3b38e1909f77fd70628eb45b9fa6bf23b6a
pavel081215 <pavelzag12@gmail.com>
2017-09-06 20:44:09 +0300

    06/09/17 23.43

---
+++ p0491_simpleadaptercustom1/src/main/java/com/startandroid/p0491_simpleadaptercustom1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.startandroid.p0491_simpleadaptercustom1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
